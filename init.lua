b694c9f859d4dd1cf564f004e113be52b4b4e106
eshtokin <eshtoki@gmail.com>
2022-07-10 13:10:38 +0300

    add packer

--- init.lua
+++ init.lua
@@ -6,8 +6,11 @@ vim.g.localleader = "\\"
 require('vars')
 require('options')
 require('keys')
--- require('plug')
+require('plug')
 
 -- vim.g --> vim.api.nvim_set_var - set global variebles
 -- vim.o --> vim.api.nvim_win_set_var - set variebles scoped to a given window
 -- vim.b --> vim.api.nvim_buf_set_var - set variebles scoped to a given buffer
+
+require('nvim-tree').setup{}
+
--- lua/vars.lua
+++ lua/vars.lua
@@ -1,3 +1,7 @@
 local g = vim.g
 g.t_co = 256			-- enable support of 256 colors
 g.background = 'dark'
+
+
+local packer_path = vim.fn.stdpath('config') .. '/site'
+vim.o.packpath = vim.o.packpath .. ',' .. packer_path
