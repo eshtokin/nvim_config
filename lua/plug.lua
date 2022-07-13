99c8cecc122e1b5ff44ad053e99daef7243337a0
eshtokin <eshtoki@gmail.com>
2022-07-13 10:18:06 +0300

    install Treesitter

--- lua/plug.lua
+++ lua/plug.lua
@@ -51,11 +51,17 @@ function(use)
     use { 'windwp/nvim-autopairs' }
 
     -- Language support protocol
-use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+
+    -- TreeSitter
+     use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
 
     -- Float terminal window
     use 'doums/floaterm.nvim'
